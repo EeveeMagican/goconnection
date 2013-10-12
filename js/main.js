4457e0b31534376919b97931cacf37cb9e3da5f7
EeveeM <eeveemgo@gmail.com>
2013-10-11 21:12:45 -0600

    fixed step1
    fixed step 1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+ // Create a YUI sandbox on your page.
+YUI().use('node', 'event', function {
+    // The Node and Event modules are loaded and ready to use.
+    // Your code goes here!
+    
+    Y.one("#helloDiv").setHTML('Goodbye!');
+    
+    var b_canvas = Y.one("#myCanvas").getDOMNode();
+    var b_context = b_canvas.getContext("2d");
+    b_context.fillRect(50, 25, 150, 100);
